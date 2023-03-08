6a6caece574fc87d541d4db23889d07b97f02c09
Calvin Walzel <calvin@pixelhub.nl>
2023-03-09 00:34:41 +0100

    Initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,7 @@
+import "index.css"
+import "syntax-highlighting.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
