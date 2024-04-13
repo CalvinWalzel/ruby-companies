c5ae3b8c0012b3c1e64c3fa07e1818c9404a563d
Calvin Walzel <calvin@pixelhub.nl>
2024-04-13 19:24:27 +0200

    Remove importmap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,5 +8,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#show"
 end
