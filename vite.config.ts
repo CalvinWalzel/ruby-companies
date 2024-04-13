4fd355c98affb006ffdeb7f33e3afeb7d2543346
Calvin Walzel <calvin@pixelhub.nl>
2024-04-13 20:51:44 +0200

    Setup Tailwind and `@` path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
 import ReactPlugin from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [RubyPlugin(), ReactPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app/frontend"),
+    },
+  },
 });
