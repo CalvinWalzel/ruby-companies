194f356baec2fbc5268c186160752d93592730b3
Calvin Walzel <calvin@pixelhub.nl>
2024-04-13 20:40:17 +0200

    Setup Vite React plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import ReactPlugin from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+  plugins: [RubyPlugin(), ReactPlugin()],
+});
