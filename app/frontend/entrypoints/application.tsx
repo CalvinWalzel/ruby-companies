f62a3e4dc1ae9f7a2c3ddfeefc595f33eab2c46e
Calvin Walzel <calvin@pixelhub.nl>
2024-04-18 09:36:19 +0200

    Add missing stylesheet import

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -2,6 +2,8 @@ import { createInertiaApp } from "@inertiajs/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@/stylesheets/globals.css";
+
 createInertiaApp({
   resolve: (name) => {
     const pages = import.meta.glob("../pages/**/*.tsx", { eager: true });
