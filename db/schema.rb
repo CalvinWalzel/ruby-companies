14e2d1504ea84bfd1e4e6fd6bed3486a4bebb091
Calvin Walzel <calvin@pixelhub.nl>
2024-08-16 17:03:20 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_16_124938) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "introduction"
     t.string "careers_page"
     t.text "description"
   end
