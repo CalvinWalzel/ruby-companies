fab1b31bacfc2593534c25e5ce861d4e858632b9
Calvin Walzel <calvin@pixelhub.nl>
2024-08-16 12:02:08 +0200

    Setup instance props and default inertia rendering

--- app/controllers/inertia_controller.rb
+++ app/controllers/inertia_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class InertiaController < ApplicationController
+  use_inertia_instance_props
+
+  after_action :set_default_inertia_rendering
+
+  private
+
+  def set_default_inertia_rendering
+    inertia_render(inertia: true) unless performed?
+  end
+
   def paginate(pagy)
     pagination = pagy_metadata(pagy)
     @pagination = PaginationSerializer.render(pagination)
