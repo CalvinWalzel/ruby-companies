127aaea7eb7707808b5fa2bf6db9ca20bcaa0cdc
Calvin Walzel <calvin@pixelhub.nl>
2024-08-17 15:47:25 +0200

    Address rubocop linting issues

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Define an application-wide content security policy.
@@ -12,7 +14,9 @@
 #     policy.object_src  :none
 #     policy.script_src  :self, :https
 #     # Allow @vite/client to hot reload javascript changes in development
-#     policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
+#     if Rails.env.development?
+#       policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }"
+#     end
 
 #     # You may need to enable this in production as well depending on your setup.
 #     policy.script_src *policy.script_src, :blob if Rails.env.test?
