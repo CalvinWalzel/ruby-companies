9ea4cb383bb9d2cf6c3444606caaca87287240e3
Calvin Walzel <calvin@pixelhub.nl>
2024-09-14 00:26:19 +0200

    Add application layout & refactor companies index page

---
+++ app/frontend/layouts/application.tsx
@@ -0,0 +1,22 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+function Layout({ children }: Props) {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-white shadow">
+        <div className="container mx-auto py-6">
+          <h1 className="text-3xl font-bold text-red-600">Ruby Companies</h1>
+        </div>
+      </header>
+      <main>
+        <div className="container mx-auto pt-5 pb-10">{children}</div>
+      </main>
+    </div>
+  );
+}
+
+Layout.displayName = "layouts/application";
+
+export default Layout;
--- app/frontend/pages/companies/index.tsx
+++ app/frontend/pages/companies/index.tsx
@@ -1,9 +1,10 @@
-import { Link } from "@inertiajs/react";
 import { ColumnDef } from "@tanstack/react-table";
 import { useMemo } from "react";
 
 import { DataTable } from "@/components/data_table";
 import ExternalLink from "@/components/external_link";
+import Link from "@/components/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import type { Company } from "@/types/company";
 import type { PaginationData } from "@/types/pagination_data";
@@ -45,14 +46,18 @@ function Index({ companies, pagination }: Props) {
   );
 
   return (
-    <div className="container mx-auto py-10">
-      <h1 className="my-4 text-xl font-bold">Companies</h1>
-      <DataTable
-        columns={columns}
-        data={companies}
-        pagination={{ ...pagination, only: ["companies", "pagination"] }}
-      />
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Companies</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <DataTable
+          columns={columns}
+          data={companies}
+          pagination={{ ...pagination, only: ["companies", "pagination"] }}
+        />
+      </CardContent>
+    </Card>
   );
 }
 
