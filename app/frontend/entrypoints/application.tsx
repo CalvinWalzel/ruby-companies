b850fb983c47f24e06971a33dd8e804f7d5b209c
Calvin Walzel <calvin@pixelhub.nl>
2024-09-14 00:26:19 +0200

    Add application layout & refactor companies index page

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -4,16 +4,24 @@ import { createRoot } from "react-dom/client";
 
 import NotFoundPage from "@/pages/errors/not_found";
 
+import Layout from "@/layouts/application";
 import "@/stylesheets/globals.css";
 
 createInertiaApp({
   resolve: (name) => {
-    const pages = import.meta.glob("../pages/**/*.tsx", { eager: true });
-    const page = pages[`../pages/${name}.tsx`];
+    const pages = import.meta.glob("../pages/**/*.tsx", {
+      eager: true,
+    });
+    const page = pages[`../pages/${name}.tsx`] as any;
 
-    if (page) return page;
+    if (!page) {
+      return { default: NotFoundPage };
+    }
 
-    return NotFoundPage;
+    page.default.layout =
+      page.default.layout || ((page: any) => <Layout>{page}</Layout>);
+
+    return page;
   },
   setup({ el, App, props }) {
     createRoot(el).render(
