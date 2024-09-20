9479572090c7e2c2e9faff0ecede9c70aca9e3d4
Calvin Walzel <calvin@pixelhub.nl>
2024-09-20 19:08:20 +0200

    Improve controller specs

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -19,6 +19,52 @@ RSpec.describe(CompaniesController, type: :request) do
     it "has a valid filter declaration" do
       expect(described_class.declarations_validator).to(be_valid)
     end
+
+    it "applies filters" do
+      create(:company, name: "Ruby Corp")
+      create(:company, name: "Python Inc")
+
+      get "/companies", params: { filter: { name: "Ruby" } }
+
+      expect(inertia.props[:companies].length).to(eq(1))
+      expect(inertia.props[:companies].first[:name]).to(eq("Ruby Corp"))
+    end
+
+    it "sorts companies by name in ascending order by default" do
+      create(:company, name: "Zebra Tech")
+      create(:company, name: "Aardvark Solutions")
+
+      get "/companies"
+
+      expect(inertia.props[:companies].first[:name]).to(eq("Aardvark Solutions"))
+      expect(inertia.props[:companies].last[:name]).to(eq("Zebra Tech"))
+    end
+
+    it "includes pagination in the response" do
+      create_list(:company, 5)
+
+      get "/companies"
+
+      expect(inertia.props[:pagination]).to(be_present)
+    end
+
+    it "includes company options in the response" do
+      get "/companies"
+
+      expect(inertia.props[:options]).to(be_present)
+      expect(inertia.props[:options]).to(have_key(:continents))
+      expect(inertia.props[:options]).to(have_key(:countries))
+      expect(inertia.props[:options]).to(have_key(:regions))
+      expect(inertia.props[:options]).to(have_key(:cities))
+    end
+
+    it "assigns compact filters" do
+      get "/companies", params: { filter: { name: "Ruby", continent: "", country: "USA", region: nil, city: "  " } }
+
+      expect(inertia.props[:filters].keys.count).to(eq(2))
+      expect(inertia.props[:filters][:name]).to(eq("Ruby"))
+      expect(inertia.props[:filters][:country]).to(eq("USA"))
+    end
   end
 
   describe "#show", inertia: true do
@@ -29,6 +75,12 @@ RSpec.describe(CompaniesController, type: :request) do
 
       expect_inertia.to(render_component("companies/show"))
       expect(inertia.props[:company]).to(be_serialized_one(company))
+    end
+
+    it "returns a 404 for non-existent company" do
+      get "/companies/non-existent-slug"
+
+      expect(response).to(have_http_status(:not_found))
     end
   end
 end
